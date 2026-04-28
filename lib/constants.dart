70a5434b2b11ba9f9aef3a7410bfa475eb14d7a3
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-04-28 19:22:05 +0300

    add animations for splash

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xff3F51B5);
+const kAnimationDuration = Duration(seconds: 1);
--- lib/features/splash/view/widget/splash_view_body.dart
+++ lib/features/splash/view/widget/splash_view_body.dart
@@ -5,8 +5,28 @@ import 'package:shop_flow/core/utlis/app_font_styles.dart';
 import 'package:shop_flow/core/utlis/assets_images.dart';
 import 'package:shop_flow/core/widget/custom_elevated_button.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  double _logoOpacity = 0;
+  double _textOpacity = 0;
+  Offset _textOffset = const Offset(0, 0.4);
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _animateLogo();
+
+      Future.delayed(const Duration(milliseconds: 1200), () {
+        if (mounted) _animateText();
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,27 +35,68 @@ class SplashViewBody extends StatelessWidget {
       child: Column(
         children: [
           const Spacer(flex: 3),
-          SvgPicture.asset(
-            AssetsImages.appLogo,
-            width: MediaQuery.widthOf(context) * 0.35,
+          // logo
+          AnimatedOpacity(
+            duration: kAnimationDuration,
+            opacity: _logoOpacity,
+            curve: Curves.easeInOutCirc,
+            child: SvgPicture.asset(
+              AssetsImages.appLogo,
+              width: MediaQuery.widthOf(context) * 0.35,
+            ),
           ),
-          Text(
-            "ShopFlow",
-            style: AppFontStyles.styleBold32.copyWith(color: kPrimaryColor),
+
+          // tagline
+          AnimatedSlide(
+            duration: kAnimationDuration,
+            offset: _textOffset,
+            curve: Curves.easeOut,
+            child: AnimatedOpacity(
+              opacity: _textOpacity,
+              duration: kAnimationDuration,
+              curve: Curves.easeOut,
+              child: _buildTagline(),
+            ),
           ),
-          const SizedBox(height: 8),
-          Text(
-            "Seamless shopping for the modern lifestyle.",
-            style: AppFontStyles.styleRegular14,
-            textAlign: TextAlign.center,
-          ),
+
           const Spacer(flex: 2),
 
+          // Get started button
           CustomElevatedButton(title: "Get Started", onPressed: () {}),
 
           const Spacer(flex: 1),
         ],
       ),
     );
+  }
+
+  Column _buildTagline() {
+    return Column(
+      children: [
+        Text(
+          "ShopFlow",
+          style: AppFontStyles.styleBold32.copyWith(color: kPrimaryColor),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          "Seamless shopping for the modern lifestyle.",
+          style: AppFontStyles.styleRegular14,
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+
+  void _animateText() {
+    setState(() {
+      _textOpacity = 1;
+      _textOffset = Offset.zero;
+    });
+  }
+
+  void _animateLogo() {
+    setState(() {
+      _logoOpacity = 1;
+    });
   }
 }
