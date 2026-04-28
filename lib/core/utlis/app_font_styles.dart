cfbeefafffd5f306f88b7e5a52a7c5d4335d8cfd
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-04-28 15:24:45 +0300

    create app font styles

---
+++ lib/core/utlis/app_font_styles.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+abstract class AppFontStyles {
+
+  static const TextStyle styleBold32 = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle styleBold24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle styleBold20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+  
+  static const TextStyle styleSemiBold18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle styleBold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle styleSemiBold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle styleRegular16 = TextStyle(fontSize: 16);
+  static const TextStyle styleRegular14 = TextStyle(fontSize: 14);
+  static const TextStyle styleRegular12 = TextStyle(fontSize: 12);
+}
