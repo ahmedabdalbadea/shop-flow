d4d83721048035a4005c318f39dc3781d865f452
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-04-29 15:38:27 +0300

    create onboarding card

---
+++ lib/features/auth/view/widgets/onboarding_card.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class OnboardingCard extends StatelessWidget {
+  const OnboardingCard({super.key, required this.child});
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(32),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(color: Color(0xffF1F5F9), spreadRadius: 5, blurRadius: 6),
+        ],
+      ),
+      child: child,
+    );
+  }
+}
--- lib/features/auth/view/widgets/sign_in_view_body.dart
+++ lib/features/auth/view/widgets/sign_in_view_body.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:shop_flow/core/utlis/app_font_styles.dart';
+import 'package:shop_flow/features/auth/view/widgets/auth_input.dart';
+import 'package:shop_flow/features/auth/view/widgets/onboarding_card.dart';
 
 class SignInViewBody extends StatelessWidget {
   const SignInViewBody({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        const Spacer(),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: OnboardingCard(
+            child: Column(
+              children: [
+              
+              ],
+            ),
+          ),
+        ),
+        const Spacer(),
+      ],
+    );
   }
 }
