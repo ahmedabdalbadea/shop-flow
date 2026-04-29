a16784deb2ad37fc0eee59894c24d6a974057e84
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-04-29 12:16:29 +0300

    create sign in view body

--- lib/features/auth/view/sign_in_view.dart
+++ lib/features/auth/view/sign_in_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:shop_flow/features/auth/view/widgets/sign_in_view_body.dart';
 
 class SignInView extends StatelessWidget {
   const SignInView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(body: SignInViewBody());
   }
 }
