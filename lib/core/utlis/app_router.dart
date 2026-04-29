cfbf9cdca99139b554966a3c3b33bb848df85c52
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-04-29 12:16:59 +0300

    add sign in route

--- lib/core/utlis/app_router.dart
+++ lib/core/utlis/app_router.dart
@@ -1,15 +1,24 @@
 import 'package:go_router/go_router.dart';
+import 'package:shop_flow/features/auth/view/sign_in_view.dart';
 import 'package:shop_flow/features/splash/view/splash_view.dart';
 
 abstract class AppRouter {
-    static final router = GoRouter(
+  static const kSignInView = "/signInView";
+  static final router = GoRouter(
     routes: [
-        GoRoute(
-            path: '/',
-            builder: (context, state) {
-                return SplashView();
-                },
-        ),
+      GoRoute(
+        path: '/',
+        builder: (context, state) {
+          return const SplashView();
+        },
+      ),
+
+      GoRoute(
+        path: kSignInView,
+        builder: (context, state) {
+          return const SignInView();
+        },
+      ),
     ],
-    );
+  );
 }
