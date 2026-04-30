71a84c3108278965e39878073d440307eccc40ac
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-04-30 18:58:25 +0300

    create sign up inputs section

---
+++ lib/features/auth/view/widgets/sign_up_inputs.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:shop_flow/constants.dart';
+import 'package:shop_flow/core/utils/app_font_styles.dart';
+import 'package:shop_flow/core/widget/user_input.dart';
+
+class SignUpInputs extends StatelessWidget {
+  const SignUpInputs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Full Name", style: AppFontStyles.styleSemiBold14),
+        const SizedBox(height: 8),
+        UserInput(hint: "John Doe"),
+        const SizedBox(height: 16),
+        Text("Email", style: AppFontStyles.styleSemiBold14),
+        const SizedBox(height: 8),
+        UserInput(hint: "name@example.com"),
+        const SizedBox(height: 16),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Password", style: AppFontStyles.styleSemiBold14),
+            Text(
+              "Forgot password?",
+              style: AppFontStyles.styleMedium12.copyWith(color: kSecTextColor),
+            ),
+          ],
+        ),
+        const SizedBox(height: 8),
+        UserInput(hint: "••••••••", isPassword: true),
+      ],
+    );
+  }
+}
