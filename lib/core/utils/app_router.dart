66ac1d6f25485761ca9fb96cdffbbe08e47750d8
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-04-30 18:32:35 +0300

    create sign up and add route to app router

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,9 +1,11 @@
 import 'package:go_router/go_router.dart';
 import 'package:shop_flow/features/auth/view/sign_in_view.dart';
+import 'package:shop_flow/features/auth/view/sign_up_view.dart';
 import 'package:shop_flow/features/splash/view/splash_view.dart';
 
 abstract class AppRouter {
   static const kSignInView = "/signInView";
+  static const kSignUpView = "/signUpView";
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -17,6 +19,13 @@ abstract class AppRouter {
         path: kSignInView,
         builder: (context, state) {
           return const SignInView();
+        },
+      ),
+
+      GoRoute(
+        path: kSignUpView,
+        builder: (context, state) {
+          return const SignUpView();
         },
       ),
     ],
---
+++ lib/features/auth/view/sign_up_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:shop_flow/features/auth/view/widgets/sign_up_view_body.dart';
+
+class SignUpView extends StatelessWidget {
+  const SignUpView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SignUpViewBody());
+  }
+}
