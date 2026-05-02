19dbef7eaaa3e1f612c29efeed285a9fef04b99d
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-05-02 15:16:06 +0300

    create erro color and focus color

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xff3F51B5);
+const kFocusColor = Color(0xffE0E0E0);
+const kErrorColor = Color(0xffF44336);
 const kAnimationDuration = Duration(seconds: 1);
 const kPrimaryBorderColor = Color(0XFFC5C5D4);
 const kBorderRadiusCircular = 12.0;
