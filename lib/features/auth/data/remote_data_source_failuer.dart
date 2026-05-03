fda80b88bcee66aaeff613ceb3bbffc5ab8a71d6
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-05-03 11:44:55 +0300

    add from firebse auth exception

--- lib/features/auth/data/remote_data_source_failuer.dart
+++ lib/features/auth/data/remote_data_source_failuer.dart
@@ -1,5 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shop_flow/core/errors/failure.dart';
 
-class RemoteDataSourceFailuer extends Failure {
-  RemoteDataSourceFailuer(super.errMsg);
+class RemoteDataSourceFailure extends Failure {
+  RemoteDataSourceFailure(super.errMsg);
+
+  factory RemoteDataSourceFailure.fromFirebaseAuthException(
+    FirebaseAuthException e,
+  ) {
+    switch (e.code) {
+      case 'email-already-in-use':
+        return RemoteDataSourceFailure('Email is already in use');
+      case 'invalid-email':
+        return RemoteDataSourceFailure('Invalid email address');
+      case 'weak-password':
+        return RemoteDataSourceFailure('Password is too weak');
+      case 'user-not-found':
+        return RemoteDataSourceFailure('No user found with this email');
+      case 'wrong-password':
+        return RemoteDataSourceFailure('Incorrect password');
+      case 'network-request-failed':
+        return RemoteDataSourceFailure('No internet connection');
+      default:
+        return RemoteDataSourceFailure(e.message ?? 'Unknown error');
+    }
+  }
 }
