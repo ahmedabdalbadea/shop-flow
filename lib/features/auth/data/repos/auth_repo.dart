f60af2600b48794d7e0a8043b08a3f77d589b864
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-05-03 11:45:35 +0300

    add sign up

--- lib/features/auth/data/repos/auth_repo.dart
+++ lib/features/auth/data/repos/auth_repo.dart
@@ -1,1 +1,10 @@
-abstract class AuthRepo {}
+import 'package:dartz/dartz.dart';
+import 'package:shop_flow/core/errors/failure.dart';
+import 'package:shop_flow/features/auth/data/models/user_model.dart';
+
+abstract class AuthRepo {
+  Future<Either<Failure, UserModel>> signUp({
+    required String email,
+    required String password,
+  });
+}
--- lib/features/auth/data/repos/auth_repo_impl.dart
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -1,3 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shop_flow/core/errors/failure.dart';
+import 'package:shop_flow/features/auth/data/auth_remote_data_source.dart';
+import 'package:shop_flow/features/auth/data/models/user_model.dart';
+import 'package:shop_flow/features/auth/data/remote_data_source_failuer.dart';
 import 'package:shop_flow/features/auth/data/repos/auth_repo.dart';
 
-class AuthRepoImpl implements AuthRepo {}
+class AuthRepoImpl implements AuthRepo {
+  final AuthRemoteDataSource _authRemoteDataSource;
+
+  AuthRepoImpl(this._authRemoteDataSource);
+  @override
+  Future<Either<Failure, UserModel>> signUp({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final credential = await _authRemoteDataSource.signUp(email, password);
+      return right(UserModel.fromUserCredential(credential));
+    } on FirebaseAuthException catch (e) {
+      return left(RemoteDataSourceFailure.fromFirebaseAuthException(e));
+    } catch (e) {
+      return left(
+        RemoteDataSourceFailure("Unexpected error, please try again"),
+      );
+    }
+  }
+}
