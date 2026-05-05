360c103f1dc011dd27106c3049cd5bdeddf087f7
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-05-05 14:47:10 +0300

    add sign in with google

--- lib/features/auth/data/auth_remote_data_source.dart
+++ lib/features/auth/data/auth_remote_data_source.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthRemoteDataSource {
   final FirebaseAuth _auth;
@@ -10,5 +11,26 @@ class AuthRemoteDataSource {
       email: email,
       password: password,
     );
+  }
+
+  Future<UserCredential?> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignIn googleSignIn = GoogleSignIn.instance;
+    await googleSignIn.initialize(
+      clientId:
+          "524858853482-99m6n6mmhm7efrfh66rcbpvpl1do03pa.apps.googleusercontent.com",
+    );
+    final GoogleSignInAccount? googleUser = await googleSignIn.authenticate();
+    if (googleUser == null) return null;
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth = googleUser.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      idToken: googleAuth.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await _auth.signInWithCredential(credential);
   }
 }
