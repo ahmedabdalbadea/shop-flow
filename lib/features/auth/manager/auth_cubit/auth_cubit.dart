6fd5c23be8cf0c01dc2c54dda747eff1cd533d9f
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-05-05 16:26:05 +0300

    create sign in in cubit

---
+++ lib/features/auth/manager/auth_cubit/auth_cubit.dart
@@ -0,0 +1,51 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:shop_flow/features/auth/data/models/user_model.dart';
+import 'package:shop_flow/features/auth/data/repos/auth_repo.dart';
+
+part 'auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit(this._authRepo) : super(AuthInitial());
+  final AuthRepo _authRepo;
+  Future<void> signUp({required String email, required String password}) async {
+    emit(SignUpLoading());
+    var result = await _authRepo.signUp(email: email, password: password);
+
+    result.fold(
+      (failure) {
+        emit(SignUpFailure(failure.errMsg));
+      },
+      (user) {
+        emit(SignUpSuccess(user: user));
+      },
+    );
+  }
+
+  Future<void> signIn({required String email, required String password}) async {
+    emit(SignInLoading());
+    var result = await _authRepo.signIn(email: email, password: password);
+
+    result.fold(
+      (failure) {
+        emit(SignInFailure(failure.errMsg));
+      },
+      (user) {
+        emit(SignInSuccess(user: user));
+      },
+    );
+  }
+
+  Future<void> signInWithGoogle() async {
+    var result = await _authRepo.signInWithGoogle();
+
+    result.fold(
+      (failure) {
+        emit(GoogleSignInFailure(failure.errMsg));
+      },
+      (user) {
+        emit(GoogleSignInSuccess(user: user));
+      },
+    );
+  }
+}
