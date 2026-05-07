12408af113c910fdc159b1a6f3031fb798bcfcfd
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-05-07 11:05:45 +0300

    add HomeViewBody with greeting and discover style header

--- lib/features/home/view/widget/home_view_body.dart
+++ lib/features/home/view/widget/home_view_body.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:shop_flow/constants.dart';
+import 'package:shop_flow/core/utils/app_font_styles.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.only(left: 24.0),
+      child: CustomScrollView(
+        slivers: [
+          const SliverToBoxAdapter(child: SizedBox(height: 32)),
+          SliverToBoxAdapter(
+            child: Text(
+              "Hello, Julian",
+              style: AppFontStyles.styleSemiBold14.copyWith(
+                color: kThrTextColor,
+              ),
+            ),
+          ),
+          const SliverToBoxAdapter(child: SizedBox(height: 8)),
+          SliverToBoxAdapter(
+            child: Text(
+              "Discover your style",
+              style: AppFontStyles.styleSemiBold24,
+            ),
+          ),
+          const SliverToBoxAdapter(child: SizedBox(height: 16)),
+        ],
+      ),
+    );
   }
 }
