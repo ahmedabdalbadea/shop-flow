65aadd912bf8e6d49339c53edbf13eeae51401a2
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-05-09 12:47:16 +0300

    create featured product card info

---
+++ lib/features/home/view/widget/featured_product_card_info.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:shop_flow/constants.dart';
+import 'package:shop_flow/core/utils/app_font_styles.dart';
+import 'package:shop_flow/features/home/view/widget/custom_icon_button.dart';
+
+class FeaturedProductCardInfo extends StatelessWidget {
+  const FeaturedProductCardInfo({
+    super.key,
+    required this.category,
+    required this.title,
+    required this.price,
+    required this.onAddTap,
+  });
+  final String category;
+  final String title;
+  final String price;
+  final VoidCallback onAddTap;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16, bottom: 24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 16),
+          Text(
+            category.toUpperCase(),
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: AppFontStyles.styleMedium12,
+          ),
+          Text(
+            title,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: AppFontStyles.styleSemiBold14,
+          ),
+          const SizedBox(height: 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "\$$price",
+                style: AppFontStyles.styleSemiBold20.copyWith(
+                  color: kSecTextColor,
+                ),
+              ),
+              CustomIconButton(onPressed: onAddTap),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
