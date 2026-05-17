0a09e4a0d9158dddc1465530740ae9d3f8106a7d
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-05-17 06:35:30 +0300

    create home repo

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:shop_flow/core/errors/failure.dart';
+import 'package:shop_flow/features/home/data/models/products/products.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, Products>> fetchProductsByCategory({
+    required String category,
+  });
+}
---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:shop_flow/core/errors/failure.dart';
+import 'package:shop_flow/core/errors/server_failure.dart';
+import 'package:shop_flow/core/utils/api_service.dart';
+import 'package:shop_flow/features/home/data/models/products/products.dart';
+import 'package:shop_flow/features/home/data/repos/home_repo.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  final ApiService _apiService;
+
+  HomeRepoImpl(this._apiService);
+  @override
+  Future<Either<Failure, Products>> fetchProductsByCategory({
+    required String category,
+  }) async {
+    try {
+      Map<String, dynamic> data = await _apiService.get(
+        endPoints: "category/$category?sortBy=rating&order=desc",
+      );
+
+      return right(Products.fromJson(data));
+    } on Exception catch (e) {
+      if (e is DioException) {
+        return left(ServerFailuer.fromDioException(e));
+      } else {
+        return left(ServerFailuer("An error occurred, please try again"));
+      }
+    }
+  }
+}
