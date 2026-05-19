aa3aade3163a9c7570661b80aad225e9b4ad3227
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-05-19 10:05:42 +0300

    create custom all products loading

---
+++ lib/features/home/view/widget/custom_all_product_loading.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:shop_flow/features/home/data/models/products/product.dart';
+import 'package:shop_flow/features/home/view/widget/all_product_card.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+class CustomAllProductLoading extends StatelessWidget {
+  const CustomAllProductLoading({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Skeletonizer.sliver(
+      child: SliverGrid.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+        childAspectRatio: 1 / 1.10,
+        children: List.generate(
+          4,
+          (index) => AllProductCard(
+            product: Product(title: "titel title title title", price: 12.12),
+          ),
+        ),
+      ),
+    );
+  }
+}
