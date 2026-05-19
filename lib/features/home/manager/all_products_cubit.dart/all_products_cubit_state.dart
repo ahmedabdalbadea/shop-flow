4343e9dc022a7778a143679c8eee810cb6f3cd2d
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-05-19 10:01:12 +0300

    determine all products cubit state

---
+++ lib/features/home/manager/all_products_cubit.dart/all_products_cubit_state.dart
@@ -0,0 +1,20 @@
+part of 'all_products_cubit_cubit.dart';
+
+@immutable
+sealed class AllProductsState {}
+
+final class AllProductsInitial extends AllProductsState {}
+
+final class AllProductsLoading extends AllProductsState {}
+
+final class AllProductsSuccess extends AllProductsState {
+  final Products products;
+
+  AllProductsSuccess({required this.products});
+}
+
+final class AllProductsFailure extends AllProductsState {
+  final String errMsg;
+
+  AllProductsFailure({required this.errMsg});
+}
