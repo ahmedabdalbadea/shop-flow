4eab1a66e7a526ceda150cbe6933a8470359060f
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-05-19 10:05:12 +0300

    provide top products cubit to home

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shop_flow/core/utils/api_service.dart';
 import 'package:shop_flow/features/home/data/repos/home_repo_impl.dart';
+import 'package:shop_flow/features/home/manager/all_products_cubit.dart/all_products_cubit.dart';
 import 'package:shop_flow/features/home/manager/top_products_cubit/top_products_cubit.dart';
 import 'package:shop_flow/features/home/view/widget/home_view_body.dart';
 
@@ -12,9 +13,19 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocProvider(
-        create: (context) =>
-            TopProductsCubit(HomeRepoImpl(ApiService(Dio())))..getTopProducts(),
+      body: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) =>
+                TopProductsCubit(HomeRepoImpl(ApiService(Dio())))
+                  ..getTopProducts(),
+          ),
+          BlocProvider(
+            create: (context) =>
+                AllProductsCubit(HomeRepoImpl(ApiService(Dio())))
+                  ..getAllProducts(),
+          ),
+        ],
         child: HomeViewBody(),
       ),
     );
