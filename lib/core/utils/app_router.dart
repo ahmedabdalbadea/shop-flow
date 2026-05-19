143412a5fd782c3b085e35cc1fbdb7c6a81b712e
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-05-19 10:35:23 +0300

    add route search view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,15 +1,15 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:shop_flow/features/auth/manager/auth_cubit/auth_cubit.dart';
 import 'package:shop_flow/features/auth/view/sign_in_view.dart';
 import 'package:shop_flow/features/auth/view/sign_up_view.dart';
 import 'package:shop_flow/features/home/view/home_view.dart';
+import 'package:shop_flow/features/home/view/search_view.dart';
 import 'package:shop_flow/features/splash/view/splash_view.dart';
 
 abstract class AppRouter {
   static const kSignInView = "/signInView";
   static const kSignUpView = "/signUpView";
   static const kHomeView = "/homeView";
+  static const kSearchView = "/searchView";
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -18,7 +18,6 @@ abstract class AppRouter {
           return const SplashView();
         },
       ),
-
       GoRoute(
         path: kSignInView,
         builder: (context, state) {
@@ -37,6 +36,13 @@ abstract class AppRouter {
         path: kHomeView,
         builder: (context, state) {
           return const HomeView();
+        },
+      ),
+
+      GoRoute(
+        path: kSearchView,
+        builder: (context, state) {
+          return const SearchView();
         },
       ),
     ],
