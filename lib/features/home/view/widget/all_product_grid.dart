83114bfc5ee787bd88c5aec939a2f521911d65c3
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-05-19 10:06:56 +0300

    create all product grid and trigger all product cubit with it

---
+++ lib/features/home/view/widget/all_product_grid.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_flow/features/home/manager/all_products_cubit.dart/all_products_cubit.dart';
+import 'package:shop_flow/features/home/view/widget/all_product_card.dart';
+import 'package:shop_flow/features/home/view/widget/custom_all_product_loading.dart';
+import 'package:shop_flow/features/home/view/widget/custom_error.dart';
+
+class AllProductsGrid extends StatelessWidget {
+  const AllProductsGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<AllProductsCubit, AllProductsState>(
+      builder: (context, state) {
+        if (state is AllProductsSuccess) {
+          return SliverGrid.count(
+            crossAxisCount: 2,
+            crossAxisSpacing: 16,
+            mainAxisSpacing: 16,
+            childAspectRatio: 1 / 1.10,
+            children: List.generate(
+              4,
+              (index) =>
+                  AllProductCard(product: state.products.products![index]),
+            ),
+          );
+        } else if (state is AllProductsFailure) {
+          return SliverToBoxAdapter(child: CustomError(errMsg: state.errMsg));
+        } else {
+          return CustomAllProductLoading();
+        }
+      },
+    );
+  }
+}
--- lib/features/home/view/widget/all_product_section.dart
+++ lib/features/home/view/widget/all_product_section.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shop_flow/core/utils/assets.dart';
-import 'package:shop_flow/features/home/view/widget/all_product_card.dart';
+import 'package:shop_flow/features/home/view/widget/all_product_grid.dart';
 import 'package:shop_flow/features/home/view/widget/home_section_header.dart';
 
 class AllProductSection extends StatelessWidget {
@@ -19,20 +18,7 @@ class AllProductSection extends StatelessWidget {
           ),
         ),
 
-        SliverGrid.count(
-          crossAxisCount: 2,
-          crossAxisSpacing: 16,
-          mainAxisSpacing: 16,
-          childAspectRatio: 1 / 1.10,
-          children: List.generate(
-            4,
-            (index) => AllProductCard(
-              image: Assets.imagesFeaturedImageTest,
-              title: "Retro Cam Mini",
-              price: "89.00",
-            ),
-          ),
-        ),
+        AllProductsGrid(),
       ],
     );
   }
