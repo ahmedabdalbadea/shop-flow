8bdab930222e9df44b1c80a1c7e6d029a4be665e
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-05-23 07:42:44 +0300

    create app bar of all products view

--- lib/features/home/view/widget/all_products_view_body.dart
+++ lib/features/home/view/widget/all_products_view_body.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:shop_flow/features/home/view/widget/custom_sliver_app_bar.dart';
 
 class AllProductsViewBody extends StatelessWidget {
   const AllProductsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return CustomScrollView(
+      slivers: [
+        CustomSliverAppBar(title: "All Products"),
+        const SliverToBoxAdapter(child: SizedBox(height: 16)),
+      ],
+    );
   }
 }
