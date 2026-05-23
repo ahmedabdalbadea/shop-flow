feee6a999bfd536b1ee67533575ec837e8856bb1
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-05-23 09:24:36 +0300

    make product details view recive product

--- lib/features/home/view/product_details_view.dart
+++ lib/features/home/view/product_details_view.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:shop_flow/features/home/data/models/products/product.dart';
 import 'package:shop_flow/features/home/view/widget/product_details_view_body.dart';
 
 class ProductDetailsView extends StatelessWidget {
-  const ProductDetailsView({super.key});
-
+  const ProductDetailsView({super.key, required this.product});
+  final Product product;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: ProductDetailsViewBody()));
+    return Scaffold(
+      body: SafeArea(child: ProductDetailsViewBody(product: product)),
+    );
   }
 }
--- lib/features/home/view/widget/product_details_view_body.dart
+++ lib/features/home/view/widget/product_details_view_body.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:shop_flow/features/home/data/models/products/product.dart';
 
 class ProductDetailsViewBody extends StatelessWidget {
-  const ProductDetailsViewBody({super.key});
-
+  const ProductDetailsViewBody({super.key, required this.product});
+  final Product product;
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
