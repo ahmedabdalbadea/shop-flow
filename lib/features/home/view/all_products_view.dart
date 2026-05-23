2e0ad603ee855516dccb81b535bdadc688595cb2
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-05-23 06:35:25 +0300

    create all products view

---
+++ lib/features/home/view/all_products_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:shop_flow/features/home/view/widget/all_products_view_body.dart';
+
+class AllProductsView extends StatelessWidget {
+  const AllProductsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: AllProductsViewBody());
+  }
+}
