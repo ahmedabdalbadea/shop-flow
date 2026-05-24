42caf4a5ff1733f6e9f1eaff3118fe5f80a3efba
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-05-24 12:34:46 +0300

    create product view body bar

--- lib/features/home/view/widget/product_details_view_body.dart
+++ lib/features/home/view/widget/product_details_view_body.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:shop_flow/features/home/data/models/products/product.dart';
+import 'package:shop_flow/features/home/view/widget/custom_sliver_app_bar.dart';
 
 class ProductDetailsViewBody extends StatelessWidget {
   const ProductDetailsViewBody({super.key, required this.product});
   final Product product;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return CustomScrollView(
+      scrollBehavior: ScrollBehavior().copyWith(
+        physics: BouncingScrollPhysics(),
+        overscroll: false,
+      ),
+      slivers: [CustomSliverAppBar(title: "Shop Flow")],
+    );
   }
 }
