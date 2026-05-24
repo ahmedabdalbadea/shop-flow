f6bdef194c3224e03def639760c0a2333c7fd266
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-05-24 12:38:34 +0300

    create product highlights

---
+++ lib/features/home/view/widget/product_highlights.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:shop_flow/features/home/data/models/products/product.dart';
+import 'package:shop_flow/features/home/view/widget/info_card.dart';
+
+class ProductHighlights extends StatelessWidget {
+  const ProductHighlights({super.key, required this.product});
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: InfoCard(
+            icon: Icons.inventory_2_outlined,
+            title: product.availabilityStatus!,
+            subTitle: product.shippingInformation!,
+          ),
+        ),
+        const SizedBox(width: 16),
+        Expanded(
+          child: InfoCard(
+            icon: Icons.verified_user_outlined,
+            title: product.warrantyInformation!,
+            subTitle: product.returnPolicy!,
+          ),
+        ),
+      ],
+    );
+  }
+}
