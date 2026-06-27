16120fd22bf788bd3996fce2cba92249eeb3a392
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-06-27 19:01:00 +0300

    resolve null check crash in UserModel fromUserCredential factory

--- lib/features/auth/data/models/user_model.dart
+++ lib/features/auth/data/models/user_model.dart
@@ -2,16 +2,16 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class UserModel {
   final String uId;
-  final String name;
+  final String? name;
   final String email;
 
-  const UserModel({required this.uId, required this.email, required this.name});
+  const UserModel({required this.uId, required this.email, this.name});
 
   factory UserModel.fromUserCredential(UserCredential credential) {
     return UserModel(
       uId: credential.user!.uid,
       email: credential.user!.email!,
-      name: credential.user!.displayName!,
+      name: credential.user!.displayName ?? "user",
     );
   }
 
