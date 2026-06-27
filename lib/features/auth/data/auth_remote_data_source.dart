9369f2f21ab7e4ba6c58e79d11087158e8f5f9cf
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-06-27 18:15:53 +0300

    create add user to collection method

--- lib/features/auth/data/auth_remote_data_source.dart
+++ lib/features/auth/data/auth_remote_data_source.dart
@@ -1,20 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shop_flow/constants.dart';
+import 'package:shop_flow/features/auth/data/models/user_model.dart';
 
 class AuthRemoteDataSource {
   final FirebaseAuth _auth;
 
-  AuthRemoteDataSource(this._auth);
+  final FirebaseFirestore _firestore;
+
+  AuthRemoteDataSource(this._auth, this._firestore);
 
   Future<UserCredential> signUp(String email, String password) async {
     return await _auth.createUserWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-  }
-
-  Future<UserCredential> signIn(String email, String password) async {
-    return await _auth.signInWithEmailAndPassword(
       email: email,
       password: password,
     );
@@ -27,8 +25,7 @@ class AuthRemoteDataSource {
       clientId:
           "524858853482-99m6n6mmhm7efrfh66rcbpvpl1do03pa.apps.googleusercontent.com",
     );
-    final GoogleSignInAccount? googleUser = await googleSignIn.authenticate();
-    if (googleUser == null) return null;
+    final GoogleSignInAccount googleUser = await googleSignIn.authenticate();
     // Obtain the auth details from the request
     final GoogleSignInAuthentication googleAuth = googleUser.authentication;
 
@@ -39,5 +36,24 @@ class AuthRemoteDataSource {
 
     // Once signed in, return the UserCredential
     return await _auth.signInWithCredential(credential);
+  }
+
+  Future<UserCredential> signIn(String email, String password) async {
+    return await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> addUserToCollection({
+    required String uId,
+    required String name,
+    required String email,
+  }) async {
+    UserModel user = UserModel(uId: uId, name: name, email: email);
+    return await _firestore
+        .collection(kUsersCollection)
+        .doc(uId)
+        .set(user.toJson());
   }
 }
