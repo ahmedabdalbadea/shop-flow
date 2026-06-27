2ebebb45ec261646a6e1532f67ad50ad946a80dd
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-06-27 18:17:27 +0300

    add firebase firestore instance to auth remote data soure local service

--- lib/core/utils/get_it.dart
+++ lib/core/utils/get_it.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
@@ -10,7 +11,8 @@ import 'package:shop_flow/features/home/data/repos/home_repo_impl.dart';
 final getIt = GetIt.instance;
 void setupServiceLocator() {
   getIt.registerLazySingleton<AuthRemoteDataSource>(
-    () => AuthRemoteDataSource(FirebaseAuth.instance),
+    () =>
+        AuthRemoteDataSource(FirebaseAuth.instance, FirebaseFirestore.instance),
   );
   getIt.registerLazySingleton<AuthRepo>(
     () => AuthRepoImpl(getIt.get<AuthRemoteDataSource>()),
