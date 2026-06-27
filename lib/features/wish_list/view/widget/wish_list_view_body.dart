9e5a4e153716de28951755b5b9b6893f00bd02a1
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-06-27 15:00:09 +0300

    add custom sliver appbar to wish llist view body

--- lib/features/wish_list/view/widget/wish_list_view_body.dart
+++ lib/features/wish_list/view/widget/wish_list_view_body.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:shop_flow/features/home/view/widget/custom_sliver_app_bar.dart';
 
 class WishListViewBody extends StatelessWidget {
   const WishListViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: []);
+    return CustomScrollView(slivers: [CustomSliverAppBar(title: "ShopFlow")]);
   }
 }
