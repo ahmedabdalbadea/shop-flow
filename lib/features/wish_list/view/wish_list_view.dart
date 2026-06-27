37e1bf81583a5890064a461ea6f165d964e85f9b
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-06-27 14:54:08 +0300

    create wish list view and body of it

---
+++ lib/features/wish_list/view/wish_list_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:shop_flow/features/wish_list/view/widget/wish_list_view_body.dart';
+
+class WishListView extends StatelessWidget {
+  const WishListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: WishListViewBody());
+  }
+}
