6b6847c34ac5d8a83ff35a6dbfadd3423ce74add
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-06-27 18:05:39 +0300

    make name non nullable and add to json

--- lib/features/auth/data/models/user_model.dart
+++ lib/features/auth/data/models/user_model.dart
@@ -1,17 +1,21 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class UserModel {
-  final String uid;
-  final String? name;
+  final String uId;
+  final String name;
   final String email;
 
-  const UserModel({required this.uid, required this.email, this.name});
+  const UserModel({required this.uId, required this.email, required this.name});
 
   factory UserModel.fromUserCredential(UserCredential credential) {
     return UserModel(
-      uid: credential.user!.uid,
+      uId: credential.user!.uid,
       email: credential.user!.email!,
-      name: credential.user?.displayName,
+      name: credential.user!.displayName!,
     );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'uId': uId, 'name': name, 'email': email};
   }
 }
