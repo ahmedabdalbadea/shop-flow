a4e1a3d203a025c07cc4224775a68bf43dc26c12
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-06-28 15:09:21 +0300

    provide user info cubit to home

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_flow/core/providers/user_provider.dart';
 import 'package:shop_flow/core/utils/service_locator.dart';
 import 'package:shop_flow/features/home/data/repos/home_repo_impl.dart';
 import 'package:shop_flow/features/home/manager/all_products_cubit.dart/all_products_cubit.dart';
 import 'package:shop_flow/features/home/manager/category_list_cubit/category_list_cubit.dart';
 import 'package:shop_flow/features/home/manager/top_products_cubit/top_products_cubit.dart';
+import 'package:shop_flow/features/home/manager/user_info_cubit/user_info_cubit.dart';
 import 'package:shop_flow/features/home/view/widget/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
@@ -27,6 +30,16 @@ class HomeView extends StatelessWidget {
             create: (context) =>
                 CategoryListCubit(getIt.get<HomeRepoImpl>())
                   ..getCategoreyList(),
+          ),
+
+          BlocProvider(
+            create: (context) => UserInfoCubit(getIt.get<HomeRepoImpl>())
+              ..getUserInfo(
+                uId: Provider.of<UserProvider>(
+                  context,
+                  listen: false,
+                ).user!.uId,
+              ),
           ),
         ],
         child: const HomeViewBody(),
