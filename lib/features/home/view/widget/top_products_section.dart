0750c4ef15201e825e189623bf64d3ea0456c7d7
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-06-28 17:45:38 +0300

    provider wish list cubit to top products list view

--- lib/features/home/view/widget/top_products_section.dart
+++ lib/features/home/view/widget/top_products_section.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_flow/core/manager/cubit/wish_list_cubit/wish_list_cubit.dart';
 import 'package:shop_flow/core/utils/app_font_styles.dart';
+import 'package:shop_flow/core/utils/service_locator.dart';
 import 'package:shop_flow/features/home/view/widget/top_products_list_view.dart';
+import 'package:shop_flow/features/wish_list/data/repos/wish_list_repo.dart';
 
 class TopProductsSection extends StatelessWidget {
   const TopProductsSection({super.key});
@@ -15,7 +19,13 @@ class TopProductsSection extends StatelessWidget {
         ),
         const SizedBox(height: 16),
 
-        SizedBox(height: 275, child: TopProductsListView()),
+        SizedBox(
+          height: 275,
+          child: BlocProvider(
+            create: (context) => WishListCubit(getIt.get<WishListRepo>()),
+            child: const TopProductsListView(),
+          ),
+        ),
         const SizedBox(height: 28),
       ],
     );
