d4954991bf0548ce3498087e60de497ffb119209
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-06-28 17:47:37 +0300

    provide wish list cubit to all product view

--- lib/features/home/view/all_products_view.dart
+++ lib/features/home/view/all_products_view.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_flow/core/manager/cubit/wish_list_cubit/wish_list_cubit.dart';
+import 'package:shop_flow/core/utils/service_locator.dart';
 import 'package:shop_flow/features/home/view/widget/all_products_view_body.dart';
+import 'package:shop_flow/features/wish_list/data/repos/wish_list_repo.dart';
 
 class AllProductsView extends StatelessWidget {
   const AllProductsView({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: SafeArea(child: AllProductsViewBody()));
+    return Scaffold(
+      body: SafeArea(
+        child: BlocProvider(
+          create: (context) => WishListCubit(getIt.get<WishListRepo>()),
+          child: const AllProductsViewBody(),
+        ),
+      ),
+    );
   }
 }
