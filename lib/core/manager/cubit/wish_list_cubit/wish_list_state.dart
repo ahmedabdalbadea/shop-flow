6cf8837f148044aef75e9f644d028423ffeb30b4
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-06-28 16:02:09 +0300

    determine wish list state

--- lib/core/manager/cubit/wish_list_cubit/wish_list_state.dart
+++ lib/core/manager/cubit/wish_list_cubit/wish_list_state.dart
@@ -4,3 +4,23 @@ part of 'wish_list_cubit.dart';
 sealed class WishListState {}
 
 final class WishListInitial extends WishListState {}
+
+final class WishListSuccess extends WishListState {
+  final Products products;
+
+  WishListSuccess(this.products);
+}
+
+final class WishListLoading extends WishListState {}
+
+final class WishListFailure extends WishListState {}
+
+final class WishListAddingSuccess extends WishListState {}
+
+final class WishListAddingLoading extends WishListState {}
+
+final class WishListAddingFailure extends WishListState {
+  final String errMsg;
+
+  WishListAddingFailure(this.errMsg);
+}
