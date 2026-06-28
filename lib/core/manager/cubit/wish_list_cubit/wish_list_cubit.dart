c73641fbc1ac0346b3a4185bfa8b9d55d854fe94
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-06-28 17:43:47 +0300

    create add product to wish list in wish list cubit

--- lib/core/manager/cubit/wish_list_cubit/wish_list_cubit.dart
+++ lib/core/manager/cubit/wish_list_cubit/wish_list_cubit.dart
@@ -1,8 +1,31 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:shop_flow/core/models/products/product.dart';
+
+import 'package:shop_flow/core/models/products/products.dart';
+import 'package:shop_flow/features/wish_list/data/repos/wish_list_repo.dart';
 
 part 'wish_list_state.dart';
 
 class WishListCubit extends Cubit<WishListState> {
-  WishListCubit() : super(WishListInitial());
+  WishListCubit(this._wishListRepo) : super(WishListInitial());
+  final WishListRepo _wishListRepo;
+  Future<void> addProductToWishList({
+    required String uId,
+    required Product product,
+  }) async {
+    emit(WishListAddingLoading());
+    var data = await _wishListRepo.addProductToWishList(
+      uId: uId,
+      product: product,
+    );
+    data.fold(
+      (failure) {
+        emit(WishListAddingFailure(failure.errMsg));
+      },
+      (_) {
+        emit(WishListAddingSuccess());
+      },
+    );
+  }
 }
