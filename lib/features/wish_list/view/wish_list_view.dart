bb871fb336e15747998604c68d981bee78f65974
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-06-29 19:02:12 +0300

    provider wish list cubit to wish list view

--- lib/features/wish_list/view/wish_list_view.dart
+++ lib/features/wish_list/view/wish_list_view.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_flow/core/manager/cubit/wish_list_cubit/wish_list_cubit.dart';
+import 'package:shop_flow/core/manager/provider/user_provider.dart';
+import 'package:shop_flow/core/utils/service_locator.dart';
+import 'package:shop_flow/features/wish_list/data/repos/wish_list_repo.dart';
 import 'package:shop_flow/features/wish_list/view/widget/wish_list_view_body.dart';
 
 class WishListView extends StatelessWidget {
@@ -6,6 +12,14 @@ class WishListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: WishListViewBody());
+    return Scaffold(
+      body: BlocProvider(
+        create: (context) => WishListCubit(getIt.get<WishListRepo>())
+          ..getWishListProducts(
+            uId: Provider.of<UserProvider>(context, listen: false).user!.uId,
+          ),
+        child: const WishListViewBody(),
+      ),
+    );
   }
 }
