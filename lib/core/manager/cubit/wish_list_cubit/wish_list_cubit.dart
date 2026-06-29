126f4a89ca6f65549b7201d757de82ef643cbf44
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-06-29 19:30:36 +0300

    create delete from wish list method in with cubit

--- lib/core/manager/cubit/wish_list_cubit/wish_list_cubit.dart
+++ lib/core/manager/cubit/wish_list_cubit/wish_list_cubit.dart
@@ -2,11 +2,11 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:dartz/dartz.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:meta/meta.dart';
 import 'package:shop_flow/core/errors/failure.dart';
 import 'package:shop_flow/core/models/products/product.dart';
 
-import 'package:shop_flow/core/models/products/products.dart';
 import 'package:shop_flow/features/wish_list/data/repos/wish_list_repo.dart';
 
 part 'wish_list_state.dart';
@@ -47,6 +47,24 @@ class WishListCubit extends Cubit<WishListState> {
         (products) => emit(WishListSuccess(products)),
       );
     });
+  }
+
+  Future<void> deleteFromWishList({
+    required String uId,
+    required Product product,
+  }) async {
+    var data = await _wishListRepo.deleteFromWishList(
+      uId: uId,
+      product: product,
+    );
+    data.fold(
+      (failure) {
+        emit(WishListFailure(failure.errMsg));
+      },
+      (_) {
+        debugPrint("============ Deleted ============");
+      },
+    );
   }
 
   @override
