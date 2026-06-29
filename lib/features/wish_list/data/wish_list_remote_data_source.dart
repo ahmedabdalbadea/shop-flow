e173b8818826ac573384d73ea668a82d6240e981
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-06-29 19:00:57 +0300

    create get wish list products in wish list remote data source

--- lib/features/wish_list/data/wish_list_remote_data_source.dart
+++ lib/features/wish_list/data/wish_list_remote_data_source.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shop_flow/constants.dart';
 import 'package:shop_flow/core/models/products/product.dart';
+import 'package:shop_flow/core/models/products/products.dart';
 
 class WishListRemoteDataSource {
   final FirebaseFirestore _firestore;
@@ -15,5 +16,14 @@ class WishListRemoteDataSource {
         .collection(kWishListProductCollection)
         .doc(uniqueDocId)
         .set({'uId': uId, ...product.toJson()});
+  }
+
+  Stream<QuerySnapshot<Map<String, dynamic>>> getWishListProducts({
+    required String uId,
+  }) {
+    return _firestore
+        .collection(kWishListProductCollection)
+        .where('uId', isEqualTo: uId)
+        .snapshots();
   }
 }
