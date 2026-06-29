3612d92de9c2ff257bf4015e17685af51bdb40cc
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-06-29 19:31:12 +0300

    trigger wish list cubit with wish list sliver grid

--- lib/features/wish_list/view/widget/wish_list_sliver_grid.dart
+++ lib/features/wish_list/view/widget/wish_list_sliver_grid.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:shop_flow/core/models/products/product.dart';
+import 'package:shop_flow/features/wish_list/view/widget/delete_icon_button.dart';
 import 'package:shop_flow/features/wish_list/view/widget/wish_list_product.dart';
 
-class WishListSliverGrid extends StatelessWidget {
+class WishListSliverGrid extends StatefulWidget {
   const WishListSliverGrid({super.key, required this.products});
   final List<Product> products;
+
+  @override
+  State<WishListSliverGrid> createState() => _WishListSliverGridState();
+}
+
+class _WishListSliverGridState extends State<WishListSliverGrid> {
+  bool appearDelete = false;
   @override
   Widget build(BuildContext context) {
     return SliverGrid(
@@ -14,13 +22,28 @@ class WishListSliverGrid extends StatelessWidget {
         mainAxisSpacing: 16,
         childAspectRatio: 1 / 1.21,
       ),
-      delegate: SliverChildBuilderDelegate(childCount: products.length, (
+      delegate: SliverChildBuilderDelegate(childCount: widget.products.length, (
         context,
         index,
       ) {
         return GestureDetector(
-          onTap: () {},
-          child: WishListProduct(product: products[index]),
+          onDoubleTap: () {
+            setState(() {
+              appearDelete = !appearDelete;
+            });
+          },
+          child: Stack(
+            children: [
+              WishListProduct(product: widget.products[index]),
+              appearDelete
+                  ? Positioned(
+                      top: 8,
+                      left: 8,
+                      child: DeleteIconButton(product: widget.products[index]),
+                    )
+                  : const SizedBox.shrink(),
+            ],
+          ),
         );
       }),
     );
