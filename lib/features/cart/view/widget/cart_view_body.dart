04a35adec40c6509fd2f2513592c60a7a6d56ec9
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-06-30 17:43:41 +0300

    add button of cart

--- lib/features/cart/view/widget/cart_view_body.dart
+++ lib/features/cart/view/widget/cart_view_body.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:shop_flow/constants.dart';
-import 'package:shop_flow/core/utils/app_font_styles.dart';
+import 'package:shop_flow/core/widget/custom_elevated_button.dart';
 import 'package:shop_flow/features/cart/view/widget/cart_products_sliver_list.dart';
 import 'package:shop_flow/features/cart/view/widget/cart_header.dart';
 import 'package:shop_flow/features/cart/view/widget/order_summary.dart';
+import 'package:shop_flow/features/home/view/widget/custom_icon_elevated_button.dart';
 import 'package:shop_flow/features/home/view/widget/custom_sliver_app_bar.dart';
 
 class CartViewBody extends StatelessWidget {
@@ -33,6 +34,39 @@ class CartViewBody extends StatelessWidget {
         ),
 
         const SliverToBoxAdapter(child: SizedBox(height: 24)),
+        const SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: SizedBox(
+              height: 56,
+              child: CustomIconElevatedButton(
+                icon: Icons.lock,
+                label: "Proceed to Checkout",
+                backgroundColor: kPrimaryColor,
+                foregroundColor: Colors.white,
+                radius: 12,
+              ),
+            ),
+          ),
+        ),
+        const SliverToBoxAdapter(child: SizedBox(height: 8)),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: SizedBox(
+              height: 56,
+              child: CustomElevatedButton(
+                title: "Continue Shopping",
+                onPressed: () {},
+                foregroundColor: kPrimaryColor,
+                borderWidth: 2,
+                borderColor: kPrimaryColor,
+              ),
+            ),
+          ),
+        ),
+
+        const SliverToBoxAdapter(child: SizedBox(height: 32)),
       ],
     );
   }
