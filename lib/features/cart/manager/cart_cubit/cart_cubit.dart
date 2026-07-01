488e0760aa29d2f715896768e4eab209e86c10bc
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-07-01 07:19:41 +0300

    determine cart cubit state

---
+++ lib/features/cart/manager/cart_cubit/cart_cubit.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:meta/meta.dart';
+
+part 'cart_state.dart';
+
+class CartCubit extends Cubit<CartState> {
+  CartCubit() : super(CartInitial());
+}
