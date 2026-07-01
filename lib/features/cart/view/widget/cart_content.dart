9f0e2e5e7e672efd243a39f3049ea96a0321624a
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-07-01 07:29:48 +0300

    trigger cart cubit with cart view body

---
+++ lib/features/cart/view/widget/cart_content.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:shop_flow/constants.dart';
+import 'package:shop_flow/core/widget/custom_elevated_button.dart';
+import 'package:shop_flow/features/cart/view/widget/cart_header.dart';
+import 'package:shop_flow/features/cart/view/widget/cart_products_sliver_list.dart';
+import 'package:shop_flow/features/cart/view/widget/order_summary.dart';
+import 'package:shop_flow/features/home/view/widget/custom_icon_elevated_button.dart';
+
+class CartContent extends StatelessWidget {
+  const CartContent({super.key, required this.productsCount});
+
+  final int productsCount;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverMainAxisGroup(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: CartHeader(title: "Your Cart", itemsCount: productsCount),
+          ),
+        ),
+        const SliverToBoxAdapter(child: SizedBox(height: 24)),
+        const CartProductsSliverList(),
+        const SliverToBoxAdapter(child: SizedBox(height: 24 - 8)),
+
+        const SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: OrderSummary(),
+          ),
+        ),
+
+        const SliverToBoxAdapter(child: SizedBox(height: 24)),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: SizedBox(
+              height: 56,
+              child: CustomIconElevatedButton(
+                icon: Icons.lock,
+                label: "Proceed to Checkout",
+                backgroundColor: kPrimaryColor,
+                foregroundColor: Colors.white,
+                onPressed: () {},
+                radius: 12,
+              ),
+            ),
+          ),
+        ),
+        const SliverToBoxAdapter(child: SizedBox(height: 8)),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: SizedBox(
+              height: 56,
+              child: CustomElevatedButton(
+                title: "Continue Shopping",
+                onPressed: () {},
+                foregroundColor: kPrimaryColor,
+                borderWidth: 2,
+                borderColor: kPrimaryColor,
+              ),
+            ),
+          ),
+        ),
+
+        const SliverToBoxAdapter(child: SizedBox(height: 32)),
+      ],
+    );
+  }
+}
--- lib/features/cart/view/widget/cart_view_body.dart
+++ lib/features/cart/view/widget/cart_view_body.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shop_flow/constants.dart';
-import 'package:shop_flow/core/widget/custom_elevated_button.dart';
-import 'package:shop_flow/features/cart/view/widget/cart_products_sliver_list.dart';
-import 'package:shop_flow/features/cart/view/widget/cart_header.dart';
-import 'package:shop_flow/features/cart/view/widget/order_summary.dart';
-import 'package:shop_flow/features/home/view/widget/custom_icon_elevated_button.dart';
+import 'package:shop_flow/core/utils/app_font_styles.dart';
+import 'package:shop_flow/features/cart/data/models/product_cart_model.dart';
+import 'package:shop_flow/features/cart/manager/cart_cubit/cart_cubit.dart';
+import 'package:shop_flow/features/cart/view/widget/cart_content.dart';
 import 'package:shop_flow/features/home/view/widget/custom_sliver_app_bar.dart';
 
 class CartViewBody extends StatelessWidget {
@@ -16,57 +16,28 @@ class CartViewBody extends StatelessWidget {
       slivers: [
         CustomSliverAppBar(title: "Shop Flow"),
         const SliverToBoxAdapter(child: SizedBox(height: 24)),
-        SliverToBoxAdapter(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24.0),
-            child: CartHeader(title: "Your Cart", itemsCount: 3),
-          ),
+        BlocBuilder<CartCubit, CartState>(
+          builder: (context, state) {
+            List<ProductCartModel> products = context
+                .read<CartCubit>()
+                .products;
+            if (products.isNotEmpty) {
+              return CartContent(productsCount: products.length);
+            } else {
+              return SliverFillRemaining(
+                hasScrollBody: false,
+                child: Center(
+                  child: Text(
+                    "No Products In Your Cart",
+                    style: AppFontStyles.styleRegular16.copyWith(
+                      color: kThrTextColor,
+                    ),
+                  ),
+                ),
+              );
+            }
+          },
         ),
-        const SliverToBoxAdapter(child: SizedBox(height: 24)),
-        const CartProductsSliverList(),
-        const SliverToBoxAdapter(child: SizedBox(height: 24 - 8)),
-
-        const SliverToBoxAdapter(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.0),
-            child: OrderSummary(),
-          ),
-        ),
-
-        const SliverToBoxAdapter(child: SizedBox(height: 24)),
-        const SliverToBoxAdapter(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.0),
-            child: SizedBox(
-              height: 56,
-              child: CustomIconElevatedButton(
-                icon: Icons.lock,
-                label: "Proceed to Checkout",
-                backgroundColor: kPrimaryColor,
-                foregroundColor: Colors.white,
-                radius: 12,
-              ),
-            ),
-          ),
-        ),
-        const SliverToBoxAdapter(child: SizedBox(height: 8)),
-        SliverToBoxAdapter(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.0),
-            child: SizedBox(
-              height: 56,
-              child: CustomElevatedButton(
-                title: "Continue Shopping",
-                onPressed: () {},
-                foregroundColor: kPrimaryColor,
-                borderWidth: 2,
-                borderColor: kPrimaryColor,
-              ),
-            ),
-          ),
-        ),
-
-        const SliverToBoxAdapter(child: SizedBox(height: 32)),
       ],
     );
   }
