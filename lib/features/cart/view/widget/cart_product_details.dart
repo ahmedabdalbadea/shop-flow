90729e9e00a798f6d1f819f0cafe57991982f74c
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-07-01 09:19:36 +0300

    add missed logic of delete in cart product details

--- lib/features/cart/view/widget/cart_product_details.dart
+++ lib/features/cart/view/widget/cart_product_details.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shop_flow/constants.dart';
+import 'package:shop_flow/core/manager/cubit/cart_cubit/cart_cubit.dart';
 import 'package:shop_flow/core/utils/app_font_styles.dart';
 import 'package:shop_flow/features/cart/data/models/product_cart_model.dart';
 import 'package:shop_flow/features/cart/view/widget/cart_icon_button.dart';
@@ -24,6 +26,9 @@ class CartProductDetails extends StatelessWidget {
                 icon: Icons.delete_outlined,
                 iconSize: 24,
                 padding: EdgeInsets.all(0),
+                onPressed: () {
+                  context.read<CartCubit>().deleteProductFromCart(product.id);
+                },
               ),
             ],
           ),
