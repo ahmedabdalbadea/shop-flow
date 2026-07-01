353df069ba396608445c40ef5e22da0d3dbd3873
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-07-01 07:21:18 +0300

    create add / deleted of cart in cart cubit

--- lib/features/cart/manager/cart_cubit/cart_cubit.dart
+++ lib/features/cart/manager/cart_cubit/cart_cubit.dart
@@ -4,8 +4,20 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:meta/meta.dart';
 
+import 'package:shop_flow/features/cart/data/cart_local_data_source.dart';
+import 'package:shop_flow/features/cart/data/models/product_cart_model.dart';
+
 part 'cart_state.dart';
 
 class CartCubit extends Cubit<CartState> {
-  CartCubit() : super(CartInitial());
+  CartCubit(this._localDataSource) : super(CartInitial());
+  final CartLocalDataSource _localDataSource;
+
+  Future<void> addProductToCart(ProductCartModel product) async {
+    await _localDataSource.addProductToCart(product);
+  }
+
+  Future<void> deleteProductFromCart(int id) async {
+    await _localDataSource.deleteProductFromCart(id);
+  }
 }
