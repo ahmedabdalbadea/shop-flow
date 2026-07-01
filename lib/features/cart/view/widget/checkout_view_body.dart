7f55ca311a879d22e200e7df64ebc5048dfe8839
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-07-01 18:59:17 +0300

    add order summary to checkout

--- lib/features/cart/view/widget/checkout_view_body.dart
+++ lib/features/cart/view/widget/checkout_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shop_flow/core/utils/app_font_styles.dart';
 import 'package:shop_flow/features/cart/view/widget/checkout_sliver_app_bar.dart';
+import 'package:shop_flow/features/cart/view/widget/order_summary.dart';
 import 'package:shop_flow/features/cart/view/widget/shipping_address_form.dart';
 
 class CheckoutViewBody extends StatelessWidget {
@@ -26,6 +27,15 @@ class CheckoutViewBody extends StatelessWidget {
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 24.0),
             child: ShippingAddressForm(),
+          ),
+        ),
+
+        const SliverToBoxAdapter(child: SizedBox(height: 24)),
+
+        const SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: OrderSummary(),
           ),
         ),
       ],
