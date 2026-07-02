e74fb9a69555e5c3abd0101addb6535c3accbf74
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-07-02 16:20:06 +0300

    add custom sliver app bar to profile

--- lib/features/profile/view/widgets/profile_view_body.dart
+++ lib/features/profile/view/widgets/profile_view_body.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:shop_flow/features/home/view/widget/custom_sliver_app_bar.dart';
 
 class ProfileViewBody extends StatelessWidget {
   const ProfileViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(slivers: []);
+    return const CustomScrollView(
+      slivers: [CustomSliverAppBar(title: "ShowFlow")],
+    );
   }
 }
