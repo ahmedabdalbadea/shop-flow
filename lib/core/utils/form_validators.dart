52129b22fc899ad6a3960f6636a7620d4d964d3d
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-07-02 14:52:54 +0300

    add vaildator to phone and address and city and state in form vaildators

--- lib/core/utils/form_validators.dart
+++ lib/core/utils/form_validators.dart
@@ -21,4 +21,40 @@ abstract class FormValidators {
     }
     return null;
   }
+
+  static String? validateInternationalPhone(String? value) {
+    final regxPhone = RegExp(r'^\+?[0-9\s\-\(\)]{9,15}$');
+
+    if (value == null || value.isEmpty) {
+      return "Phone number is required";
+    } else if (!regxPhone.hasMatch(value)) {
+      return "Please enter a valid phone number";
+    }
+
+    return null;
+  }
+
+  static String? validateCity(String? value) {
+    final cityRegExp = RegExp(r'^[a-zA-Z\s]+$');
+
+    if (value == null || value.trim().isEmpty) {
+      return 'City is required';
+    }
+    if (!cityRegExp.hasMatch(value.trim())) {
+      return 'City name should only contain letters';
+    }
+    return null;
+  }
+
+  static String? validateState(String? value) {
+    final stateRegExp = RegExp(r'^[a-zA-Z\s]{2,20}$');
+
+    if (value == null || value.trim().isEmpty) {
+      return 'State is required';
+    }
+    if (!stateRegExp.hasMatch(value.trim())) {
+      return 'Enter a valid state name or abbreviation';
+    }
+    return null;
+  }
 }
