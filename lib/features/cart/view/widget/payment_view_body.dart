0f27d0d9f035e21f922831baf3189a56ce73b8be
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-07-04 18:51:18 +0300

    create total to pay in payment

--- lib/features/cart/view/widget/payment_view_body.dart
+++ lib/features/cart/view/widget/payment_view_body.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:shop_flow/constants.dart';
+import 'package:shop_flow/core/utils/app_font_styles.dart';
 import 'package:shop_flow/features/cart/view/widget/secondary_sliver_app_bar.dart';
+import 'package:shop_flow/features/cart/view/widget/total_to_pay.dart';
 
 class PaymentViewBody extends StatelessWidget {
   const PaymentViewBody({super.key});
@@ -9,6 +12,14 @@ class PaymentViewBody extends StatelessWidget {
     return CustomScrollView(
       slivers: [
         const SecondarySliverAppBar(title: "Payment"),
+        const SliverToBoxAdapter(child: SizedBox(height: 24)),
+
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: TotalToPay(totalAmount: 267.84),
+          ),
+        ),
         const SliverToBoxAdapter(child: SizedBox(height: 24)),
       ],
     );
---
+++ lib/features/cart/view/widget/total_to_pay.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:shop_flow/constants.dart';
+import 'package:shop_flow/core/utils/app_font_styles.dart';
+
+class TotalToPay extends StatelessWidget {
+  const TotalToPay({super.key, required this.totalAmount});
+  final double totalAmount;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: kFillColor,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "TOTAL TO PAY",
+            style: AppFontStyles.styleMedium12.copyWith(color: kThrTextColor),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            "\$$totalAmount",
+            style: AppFontStyles.styleSemiBold24.copyWith(color: kSecTextColor),
+          ),
+        ],
+      ),
+    );
+  }
+}
