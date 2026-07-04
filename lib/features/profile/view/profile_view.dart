63750690c6e2adc314e834eb5cd50c7649888bc5
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-07-04 15:38:16 +0300

    provider user info cubit to profile view

--- lib/features/profile/view/profile_view.dart
+++ lib/features/profile/view/profile_view.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_flow/core/manager/cubit/user_info_cubit/user_info_cubit.dart';
+import 'package:shop_flow/core/manager/provider/user_provider.dart';
+import 'package:shop_flow/core/utils/service_locator.dart';
+import 'package:shop_flow/features/home/data/repos/home_repo_impl.dart';
 import 'package:shop_flow/features/profile/view/widgets/profile_view_body.dart';
 
 class ProfileView extends StatelessWidget {
@@ -6,6 +12,17 @@ class ProfileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: SafeArea(top: false, child: ProfileViewBody()));
+    return Scaffold(
+      body: SafeArea(
+        top: false,
+        child: BlocProvider(
+          create: (context) => UserInfoCubit(getIt.get<HomeRepoImpl>())
+            ..getUserInfo(
+              uId: Provider.of<UserProvider>(context, listen: false).user!.uId,
+            ),
+          child: const ProfileViewBody(),
+        ),
+      ),
+    );
   }
 }
