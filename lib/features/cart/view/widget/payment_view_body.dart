d12483f826d113c2cc730f3925869689134c73f7
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-07-04 19:50:40 +0300

    add pay now button

--- lib/features/cart/view/widget/payment_view_body.dart
+++ lib/features/cart/view/widget/payment_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shop_flow/constants.dart';
+import 'package:shop_flow/core/widget/custom_elevated_button.dart';
 import 'package:shop_flow/features/cart/view/widget/secondary_sliver_app_bar.dart';
 import 'package:shop_flow/features/cart/view/widget/select_payment_method.dart';
 import 'package:shop_flow/features/cart/view/widget/total_to_pay.dart';
@@ -9,6 +11,7 @@ class PaymentViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
+      physics: const ClampingScrollPhysics(),
       slivers: [
         const SecondarySliverAppBar(title: "Payment"),
         const SliverToBoxAdapter(child: SizedBox(height: 24)),
@@ -25,6 +28,21 @@ class PaymentViewBody extends StatelessWidget {
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 24.0),
             child: SelectPaymentMethod(),
+          ),
+        ),
+        const SliverToBoxAdapter(child: SizedBox(height: 24)),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: SizedBox(
+              height: 56,
+              child: CustomElevatedButton(
+                title: "Pay Now",
+                backgroundColor: kPrimaryColor,
+                foregroundColor: Colors.white,
+                onPressed: () {},
+              ),
+            ),
           ),
         ),
         const SliverToBoxAdapter(child: SizedBox(height: 24)),
