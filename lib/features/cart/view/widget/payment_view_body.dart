1c04713c2d14e0f1410dc8628c7d27ad92b252a5
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-07-04 18:40:54 +0300

    add secondary sliver app bar to payment

--- lib/features/cart/view/widget/payment_view_body.dart
+++ lib/features/cart/view/widget/payment_view_body.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:shop_flow/features/cart/view/widget/secondary_sliver_app_bar.dart';
 
 class PaymentViewBody extends StatelessWidget {
   const PaymentViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(slivers: [
-        
+    return CustomScrollView(
+      slivers: [
+        const SecondarySliverAppBar(title: "Payment"),
+        const SliverToBoxAdapter(child: SizedBox(height: 24)),
       ],
     );
   }
