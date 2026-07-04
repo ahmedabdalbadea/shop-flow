9fda58abe562ea10b9e9eb78a5fd4511efb76d4d
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-07-04 18:06:41 +0300

    create payment view

---
+++ lib/features/cart/view/payment_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:shop_flow/features/cart/view/widget/payment_view_body.dart';
+
+class PaymentView extends StatelessWidget {
+  const PaymentView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: PaymentViewBody());
+  }
+}
---
+++ lib/features/cart/view/widget/payment_view_body.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PaymentViewBody extends StatelessWidget {
+  const PaymentViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(slivers: [
+        
+      ],
+    );
+  }
+}
