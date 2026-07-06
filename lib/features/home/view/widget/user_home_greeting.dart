4a39eb6438ff9ef6bf7c7f740772514d319dcbbb
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-07-06 10:28:58 +0300

    toggle home greeting visibility after one minute

--- lib/features/home/view/widget/user_home_greeting.dart
+++ lib/features/home/view/widget/user_home_greeting.dart
@@ -6,20 +6,44 @@ import 'package:shop_flow/core/utils/app_font_styles.dart';
 import 'package:shop_flow/core/manager/cubit/user_info_cubit/user_info_cubit.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
-class UserHomeGreeting extends StatelessWidget {
+class UserHomeGreeting extends StatefulWidget {
   const UserHomeGreeting({super.key});
+
+  @override
+  State<UserHomeGreeting> createState() => _UserHomeGreetingState();
+}
+
+class _UserHomeGreetingState extends State<UserHomeGreeting> {
+  bool isVisiable = true;
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(minutes: 1), () {
+      if (mounted) {
+        setState(() {
+          isVisiable = false;
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<UserInfoCubit, UserInfoState>(
       builder: (context, state) {
         if (state is UserInfoSuccess) {
-          return Text(
-            "Hello, ${formateName(state.user.name)![0]}",
-            style: AppFontStyles.styleSemiBold14.copyWith(color: kThrTextColor),
+          return Visibility(
+            visible: isVisiable,
+            child: Text(
+              "Hello, ${formateName(state.user.name)![0]}",
+              style: AppFontStyles.styleSemiBold14.copyWith(
+                color: kThrTextColor,
+              ),
+            ),
           );
         } else if (state is UserInfoFailure) {
-          Text(
+          return Text(
             state.errMsg,
             style: AppFontStyles.styleSemiBold14.copyWith(color: kErrorColor),
           );
