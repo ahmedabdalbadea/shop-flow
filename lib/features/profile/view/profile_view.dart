b692410d998d6be7fb1f7c4c8e73b74ab21b13c8
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-07-06 09:32:06 +0300

    provider order cubit to profile view

--- lib/features/profile/view/profile_view.dart
+++ lib/features/profile/view/profile_view.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_flow/core/manager/cubit/order_cubit/order_cubit.dart';
 import 'package:shop_flow/core/manager/cubit/user_info_cubit/user_info_cubit.dart';
 import 'package:shop_flow/core/manager/provider/user_provider.dart';
 import 'package:shop_flow/core/utils/service_locator.dart';
 import 'package:shop_flow/features/home/data/repos/home_repo_impl.dart';
+import 'package:shop_flow/features/profile/data/repos/profile_repo.dart';
 import 'package:shop_flow/features/profile/view/widgets/profile_view_body.dart';
 
 class ProfileView extends StatelessWidget {
@@ -15,11 +17,27 @@ class ProfileView extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         top: false,
-        child: BlocProvider(
-          create: (context) => UserInfoCubit(getIt.get<HomeRepoImpl>())
-            ..getUserInfo(
-              uId: Provider.of<UserProvider>(context, listen: false).user!.uId,
+        child: MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) => UserInfoCubit(getIt.get<HomeRepoImpl>())
+                ..getUserInfo(
+                  uId: Provider.of<UserProvider>(
+                    context,
+                    listen: false,
+                  ).user!.uId,
+                ),
             ),
+            BlocProvider(
+              create: (context) => OrderCubit(getIt.get<ProfileRepo>())
+                ..fetchOrders(
+                  uId: Provider.of<UserProvider>(
+                    context,
+                    listen: false,
+                  ).user!.uId,
+                ),
+            ),
+          ],
           child: const ProfileViewBody(),
         ),
       ),
