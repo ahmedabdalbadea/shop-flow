19f284930cb477fb049acc44ec7bd5baf4e45eee
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-07-06 09:32:43 +0300

    trigger order cubit with total orders

--- lib/features/profile/view/widgets/total_orders.dart
+++ lib/features/profile/view/widgets/total_orders.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shop_flow/constants.dart';
+import 'package:shop_flow/core/manager/cubit/order_cubit/order_cubit.dart';
 import 'package:shop_flow/core/utils/app_font_styles.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class TotalOrders extends StatelessWidget {
   const TotalOrders({super.key});
@@ -20,9 +23,35 @@ class TotalOrders extends StatelessWidget {
             "TOTAL ORDERS",
             style: AppFontStyles.styleRegular16.copyWith(color: kThrTextColor),
           ),
-          Text(
-            "24",
-            style: AppFontStyles.styleRegular16.copyWith(color: kSecTextColor),
+          BlocBuilder<OrderCubit, OrderState>(
+            builder: (context, state) {
+              if (state is OrdersSuccess) {
+                return Text(
+                  state.orders.length.toString(),
+                  style: AppFontStyles.styleRegular16.copyWith(
+                    color: kSecTextColor,
+                  ),
+                );
+              }
+
+              if (state is OrdersFailure) {
+                return Text(
+                  'Unknown',
+                  style: AppFontStyles.styleRegular16.copyWith(
+                    color: kSecTextColor,
+                  ),
+                );
+              }
+
+              return Skeletonizer(
+                child: Text(
+                  '24',
+                  style: AppFontStyles.styleRegular16.copyWith(
+                    color: kSecTextColor,
+                  ),
+                ),
+              );
+            },
           ),
         ],
       ),
