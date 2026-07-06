e00fffa28f75a4bb8276cc8f816a6e8fa2f6411a
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-07-06 09:36:15 +0300

    trigger order cubit with order sliver lsit

--- lib/features/profile/view/widgets/order_sliver_list.dart
+++ lib/features/profile/view/widgets/order_sliver_list.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:shop_flow/features/profile/view/widgets/order_item.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_flow/core/manager/cubit/order_cubit/order_cubit.dart';
+import 'package:shop_flow/features/profile/view/widgets/order_sliver_list_content.dart';
+import 'package:shop_flow/features/profile/view/widgets/orders_sliver_list_loading.dart';
 
 class OrderSliverList extends StatelessWidget {
   const OrderSliverList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SliverList.builder(
-      itemCount: 3,
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.only(top: 16.0),
-          child: OrderItem(),
-        );
+    return BlocBuilder<OrderCubit, OrderState>(
+      builder: (context, state) {
+        if (state is OrdersSuccess) {
+          return OrderSliverListContent(orders: state.orders);
+        } else if (state is OrdersFailure) {
+          return SliverFillRemaining(
+            hasScrollBody: false,
+            child: Center(child: Text(state.errMsg)),
+          );
+        }
+        return OrdersSliverListLoading();
       },
     );
   }
